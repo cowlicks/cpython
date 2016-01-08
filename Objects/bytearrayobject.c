93950d0fcecdea734427a22ca5fde17ea774492d
Blake Griffith <blake.a.griffith@gmail.com>
2016-01-08 01:56:43 +0100

    Add xor to to PyByteArray's PyNumberMethods

--- Objects/bytearrayobject.c
+++ Objects/bytearrayobject.c
@@ -3055,10 +3055,22 @@ bytearray_mod(PyObject *v, PyObject *w)
 }
 
 static PyNumberMethods bytearray_as_number = {
-    0,              /*nb_add*/
-    0,              /*nb_subtract*/
-    0,              /*nb_multiply*/
-    bytearray_mod,  /*nb_remainder*/
+    0,                /*nb_add*/
+    0,                /*nb_subtract*/
+    0,                /*nb_multiply*/
+    bytearray_mod,    /*nb_remainder*/
+    0,                /* nb_divmod */
+    0,                /* nb_power */
+    0,                /* nb_negative */
+    0,                /* nb_positive */
+    0,                /* nb_absolute */
+    0,                /* nb_bool */
+    0,                /* nb_invert */
+    0,                /* nb_lshift */
+    0,                /* nb_rshift */
+    0,                /* nb_and */
+    PyByteArray_Xor,  /* nb_xor */
+
 };
 
 PyDoc_STRVAR(bytearray_doc,
